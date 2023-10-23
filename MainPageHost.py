7b27fe33c26baabd27c12c063a184ff21e8a762d
VijeshPethuram <92882475+VijeshPethuram@users.noreply.github.com>
2023-10-23 22:16:01 +0530

    Added Requirements and Main Page

---
+++ MainPageHost.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+
+
+st.title("Student Dropout Analysis📊")
+st.header("Welcome to our Student Dropout Analysis Project.")
+st.write("Just Switch over tabs to learn more!😀")
+st.sidebar.markdown("---")
+
