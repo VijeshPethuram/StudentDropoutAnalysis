9bc41c7cd683198164b5dbe31bdfa02dd445a3ca
VijeshPethuram <92882475+VijeshPethuram@users.noreply.github.com>
2023-10-23 22:56:14 +0530

    Update MainPageHost.py

--- MainPageHost.py
+++ MainPageHost.py
@@ -5,5 +5,5 @@ import streamlit as st
 st.title("Student Dropout Analysis📊")
 st.header("Welcome to our Student Dropout Analysis Project.")
 st.write("Just Switch over tabs to learn more!😀")
-st.sidebar.markdown("Made with ❤️ by Vijesh Pethuram K")
+st.sidebar.markdown("Made with ❤️ by Vijesh Pethuram K and team")
 
